e0d095d67f90a37b0c7a7208fc91e1f34f34621c
Vil-Blass <jeffry16snake.@hotmailcom>
2023-11-27 18:54:14 +0100

    funfionalidad al nav

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IkusMira
-trabajo del curso Marketing Digital.
+## trabajo del curso ## Marketing Digital.
+
+### https://www.canva.com/design/DAF1Fn2m4k0/YqEthVu_RUJFZF2WxnxV5g/view?utm_content=DAF1Fn2m4k0&utm_campaign=designshare&utm_medium=link&utm_source=editor
--- js/markting.js
+++ js/markting.js
@@ -2,4 +2,50 @@
 'use strict';
 
 
-// Variables globales
+// al pasare el raton por encima del texto se activa el evento mouseover
+
+
+function mostrarSubMenu() {
+    let mostrar = document.querySelector('.submenu-menu');
+  
+    addEventListener('mouseover', () => {
+      mostrar.style.display = 'block';
+      mostrar.style.backgroundColor = 'red';
+      mostrar.style.width = '100%'; 
+      mostrar.style.cursor = 'pointer';
+
+    });
+    
+    addEventListener('mouseout', () => {
+      mostrar.style.display = 'none';
+    });
+
+  }
+console.log('YO');
+let btn = document.querySelector('.botton');
+ btn.addEventListener('click', () => {
+   mostrarSubMenu();
+ })
+  
+  console.log('ESTOY');
+  function mostrarSubMenu2() {
+    let submenu2 = document.querySelector('.submenu2-menu');
+    
+    addEventListener('mouseover', () => {
+      submenu2.style.display = 'block';
+      submenu2.style.backgroundColor = 'green';
+      submenu2.style.width = 'max-content';
+      submenu2.style.cursor = 'pointer';
+    });
+  
+    addEventListener('mouseout', () => {
+      submenu2.style.display = 'none';
+    });
+  }
+console.log('AQUI');
+
+let btn2 = document.querySelector('.botton1');
+  
+  btn2.addEventListener('click', () => {
+    mostrarSubMenu2();
+  });
