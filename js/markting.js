dd8cbcc03abe832400be43a3c9cdb6988980b4e8
Vil-Blass <jeffry16snake.@hotmailcom>
2023-11-28 18:58:58 +0100

    nav itinerario5000ms

--- js/markting.js
+++ js/markting.js
@@ -10,34 +10,47 @@ function mostrarSubMenu() {
 
   mostrar.style.display = 'block';
   mostrar.style.backgroundColor = 'red';
-  mostrar.style.width = '100%';
   mostrar.style.cursor = 'pointer';
+  mostrar.style.position = 'absolute';
+  mostrar.style.zIndex = '1';
+  mostrar.style.top = '50px';
+  mostrar.style.width = 'fit-content';
+
 
 
 
 }
 
 function ocultarSubMenu() {
-
+  setTimeout(() => {
   mostrar.style.display = 'none';
-
+},5000)
 
 }
 
 function mostrarSubMenu2() {
 
+  
+
   submenu2.style.display = 'block';
   submenu2.style.backgroundColor = 'red';
-  submenu2.style.width = '100%';
   submenu2.style.cursor = 'pointer';
-  submenu2.style.animation = '2s';
+  submenu2.style.position = 'absolute';
+  submenu2.style.zIndex = '1';
+  submenu2.style.top = '50px';
+  submenu2.style.width = 'fit-content';
+  submenu2.style.animation = 'ocultar 5s';
+  
+
 
 }
 
 function ocultarSubMenu2() {
-
+setTimeout(() => {
   submenu2.style.display = 'none';
-
+  
+  
+},5000);
 }
 
 
@@ -48,13 +61,14 @@ const  btn1 = document.querySelector('.botton1');
 btn.addEventListener('mouseout', () => {
   ocultarSubMenu();
 })
-btn.addEventListener('click', () => {
+btn.addEventListener('mouseover', () => {
   mostrarSubMenu();
+
 })
 btn1.addEventListener('mouseout', () => {
   ocultarSubMenu2();
 })
-btn1.addEventListener('click', () => {
+btn1.addEventListener('mouseover', () => {
   mostrarSubMenu2();
 })
 
