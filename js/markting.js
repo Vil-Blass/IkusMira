a2f9e9ab8a28701e5e728a395ded85d6f223603c
Vil-Blass <jeffry16snake@hotmail.com>
2023-11-28 14:34:11 +0100

    pequeños cambios en el nav

--- js/markting.js
+++ js/markting.js
@@ -3,49 +3,59 @@
 
 
 // al pasare el raton por encima del texto se activa el evento mouseover
-
-
+let mostrar = document.querySelector('.submenu-menu');
+let submenu2 = document.querySelector('.submenu2-menu');
 function mostrarSubMenu() {
-    let mostrar = document.querySelector('.submenu-menu');
-  
-    addEventListener('mouseover', () => {
-      mostrar.style.display = 'block';
-      mostrar.style.backgroundColor = 'red';
-      mostrar.style.width = '100%'; 
-      mostrar.style.cursor = 'pointer';
 
-    });
-    
-    addEventListener('mouseout', () => {
-      mostrar.style.display = 'none';
-    });
 
-  }
-console.log('YO');
-let btn = document.querySelector('.botton');
- btn.addEventListener('click', () => {
-   mostrarSubMenu();
- })
-  
-  console.log('ESTOY');
-  function mostrarSubMenu2() {
-    let submenu2 = document.querySelector('.submenu2-menu');
-    
-    addEventListener('mouseover', () => {
-      submenu2.style.display = 'block';
-      submenu2.style.backgroundColor = 'green';
-      submenu2.style.width = 'max-content';
-      submenu2.style.cursor = 'pointer';
-    });
-  
-    addEventListener('mouseout', () => {
-      submenu2.style.display = 'none';
-    });
-  }
-console.log('AQUI');
+  mostrar.style.display = 'block';
+  mostrar.style.backgroundColor = 'red';
+  mostrar.style.width = '100%';
+  mostrar.style.cursor = 'pointer';
 
-let btn2 = document.querySelector('.botton1');
-  
-  btn2.addEventListener('click', () => {
-    mostrarSubMenu2();
-  });
+
+
+}
+
+function ocultarSubMenu() {
+
+  mostrar.style.display = 'none';
+
+
+}
+
+function mostrarSubMenu2() {
+
+  submenu2.style.display = 'block';
+  submenu2.style.backgroundColor = 'red';
+  submenu2.style.width = '100%';
+  submenu2.style.cursor = 'pointer';
+  submenu2.style.animation = '2s';
+
+}
+
+function ocultarSubMenu2() {
+
+  submenu2.style.display = 'none';
+
+}
+
+
+console.log('YO' + mostrar.innerHTML);
+const  btn = document.querySelector('.botton');
+const  btn1 = document.querySelector('.botton1');
+
+btn.addEventListener('mouseout', () => {
+  ocultarSubMenu();
+})
+btn.addEventListener('click', () => {
+  mostrarSubMenu();
+})
+btn1.addEventListener('mouseout', () => {
+  ocultarSubMenu2();
+})
+btn1.addEventListener('click', () => {
+  mostrarSubMenu2();
+})
+
+console.log('ESTOY');
